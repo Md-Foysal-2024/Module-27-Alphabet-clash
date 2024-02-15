72322611c74acff8bd7d83ac373e6a5e93891cc1
Md-Foysal-2024 <mdfoysal918978@gmail.com>
2024-02-15 08:07:27 +0600

    Button Has been added

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,12 +1,12 @@
 
 // function play() {
 //     const homeScreenSection = document.getElementById('home-screen');
-//     //console.log(homeScreenSection);
+//       //console.log(homeScreenSection);
 //     homeScreenSection.classList.add('hidden');
 
 
 //     const playGroundSection = document.getElementById('play-ground');
-//     //console.log(playGroundSection);
+//       //console.log(playGroundSection);
 //     playGroundSection.classList.remove('hidden');
 
 // }  
@@ -16,24 +16,74 @@
 //  এই ফাংশনের ভিতরে ভিন্ন ভিন্ন আইডি দিয়ে কল করে দেই তাহলে 
 //  কাজ অনেকটা সহজ হবে এবং বার বার করা লাগবে না শুধু আইডি দিয়ে কল করলেই হবে।
 
-function play(){
-
-    hiddenElementById('home-screen');
-    showElementById('play-ground');
-
-}
-
-
 function hiddenElementById(elementId) {
     const element = document.getElementById(elementId)
     element.classList.add('hidden');
-
 }
+
 
 function showElementById(elementId) {
     const element = document.getElementById(elementId)
     element.classList.remove('hidden');
+}
+
+
+
+
+function setBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add("bg-orange-400");
 
 }
 
 
+
+
+function getARandomAlphabet() {
+
+    //creat an Alphabet Array split('') by string---> একটি  স্ট্রিং কে split করে Array তে রূপান্তর করা হলো।
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+
+    // get a random index number between (0 to 25)--->
+    const randomNumber = Math.random() * 25
+    // console.log(randomNumber)
+    const indexNumber = Math.round(randomNumber); //  random ভগ্নাংশ নাম্বার কে রাউন্ড করা হলো।
+    // console.log(indexNumber);
+
+    //  indexNumber ও alphabets দুটোই এক সাথে --->
+    const alphabet = alphabets[indexNumber];
+    // console.log(indexNumber, alphabet);
+
+    // শুধু অক্ষর টা কে রিটার্ন করছি।
+    return alphabet;
+}
+
+
+function continueGame() {
+
+    const alphabet = getARandomAlphabet();
+    console.log('শুধু অক্ষর টা কে রিটার্ন করছি:', alphabet);
+
+    // set randomly alphabet to the screen.
+    const currentAlphabet = document.getElementById("current-alphabet");
+    currentAlphabet.innerText = alphabet;
+
+    setBackgroundColorById(alphabet);
+}
+
+
+
+
+
+
+
+
+function play() {
+
+    hiddenElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
+}
